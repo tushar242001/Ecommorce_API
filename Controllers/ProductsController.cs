e6e8195e5d0d59cd3eaa42dcff140b0a2cd51885
tushar242001 <tusharadling24@gmail.com>
2025-09-14 20:42:38 +0530

    first commit

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    
+    public class ProductsController : ControllerBase
+    {
+        
+       [HttpGet]
+       public IActionResult GetByCategory()
+        {
+            return Ok("Hello World");
+        }
+    }
+}
+
